07c21dbfe7b7bda3feff1bd6a433a8899d8e5848
Michael Kelly <puritandesigns@gmail.com>
2019-09-26 17:15:55 -0400

    Use canvass_type as type in FieldOption\Edit

--- src/CanvassLaravel/Http/Controllers/FieldOption/Edit.php
+++ src/CanvassLaravel/Http/Controllers/FieldOption/Edit.php
@@ -24,7 +24,7 @@ class Edit
             'form' => $form,
             'parent' => $parent,
             'field' => $field,
-            'type' => $field->type
+            'type' => $field->canvass_type
         ]);
     }
 }
